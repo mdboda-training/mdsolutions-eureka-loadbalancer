675aace67e392c567238564b9ce085e3c1787408
gadhilinga <boda.gadhilinga@gmail.com>
2022-06-02 19:19:43 +0530

    feign changes first commit

---
+++ src/main/java/com/mdsolutions/MdsolutionsFeignClientApplication.java
@@ -0,0 +1,13 @@
+package com.mdsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdsolutionsFeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdsolutionsFeignClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdsolutions/MdsolutionsFeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdsolutionsFeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
